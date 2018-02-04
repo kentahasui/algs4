4f39f062b14b32961c7a35648cad18a79c403dbb
Kenta Hasui <kentahasui@gmail.com>
2018-02-04 13:23:18 -0500

    Implement Point.java and unit tests for Point.
    This is part 1 of the Collinear Points project.
    Point implements the compareTo() and slopeTo() methods.

---
+++ src/main/java/Point.java
@@ -0,0 +1,159 @@
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
+
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Point implements Comparable<Point> {
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
+    /**
+     * Initializes a new point.
+     *
+     * @param  x the <em>x</em>-coordinate of the point
+     * @param  y the <em>y</em>-coordinate of the point
+     */
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        if (isDegenerate(that)) { return Double.NEGATIVE_INFINITY; }
+        if (isHorizontal(that)) { return 0.0; }
+        if (isVertical(that)) { return Double.POSITIVE_INFINITY; }
+
+        double rise = (double) that.y - y;
+        double run = (double) that.x - x;
+        return rise / run;
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        // Case 1: Both points are equal. Points should be the same.
+        if (isDegenerate(that)) { return 0; }
+
+        // Case 2: Both points are on same horizontal line. Compare by x coord.
+        if (isHorizontal(that)) { return x < that.x ? -1 : 1; }
+
+        // Case 3: Any other case. Simply compare by y coordinate.
+        return y < that.y ? -1 : 1;
+    }
+
+    /** Determines whether the given point has the same x and y coordinates */
+    private boolean isDegenerate(Point that){
+        return y == that.y && x == that.x;
+    }
+
+    /** Determines whether the given point has the same y coordinate */
+    private boolean isHorizontal(Point that){
+        return y == that.y;
+    }
+
+    /** Determines whether the given point has the same x coordinate */
+    private boolean isVertical(Point that){
+        return x == that.x;
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public Comparator<Point> slopeOrder() {
+        return new SlopeComparator();
+    }
+
+    /**
+     * Compares points based on slope to this point.
+     * Implemented as a non-static class in order to retain pointer to
+     * enclosing class.
+     */
+    private class SlopeComparator implements Comparator<Point>{
+        public int compare(Point p1, Point p2){
+            double slope1 = slopeTo(p1);
+            double slope2 = slopeTo(p2);
+
+            // If the points have the same slope to the source point,
+            // compare by y, then by x
+            if (Double.compare(slope1, slope2) == 0) {
+                return p1.compareTo(p2);
+            }
+
+            // If the points have different slopes, compare by the slopes.
+            // The smaller slope is lesser, and the larger slope is greater.
+            return Double.compare(slope1, slope2);
+        }
+    }
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+}
---
+++ src/test/java/PointTest.java
@@ -0,0 +1,234 @@
+/******************************************************************************
+ *  Compilation:  javac PointTest.java
+ *  Execution:    java PointTest
+ *  Dependencies: Point.java
+ *
+ *  Unit tests for Point.java
+ *
+ ******************************************************************************/
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class PointTest {
+    ////////////////////////////////////////////////////////////////////////////
+    // Unit tests for slopeTo()
+    ////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void slopeToSamePoint_shouldBeNegativeInfinity(){
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 1);
+        assertThat(p1.slopeTo(p2)).isNegativeInfinity();
+    }
+
+    @Test
+    public void verticalSlope_shouldBePositiveInfinity(){
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 5);
+        assertThat(p1.slopeTo(p2)).isPositiveInfinity();
+    }
+
+    @Test
+    public void horizontalSlope_shouldBeZero(){
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(10, 1);
+        assertThat(p1.slopeTo(p2)).isZero();
+    }
+
+    @Test
+    public void positiveSlope(){
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 10);
+        assertThat(p1.slopeTo(p2)).isEqualTo(9.0);
+    }
+
+    @Test
+    public void negativeSlope(){
+        Point p1 = new Point(0, 100);
+        Point p2 = new Point(10, 0);
+        assertThat(p1.slopeTo(p2)).isEqualTo(-10.0);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Unit tests for compareTo()
+    ////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void compareTo_samePoint_shouldBeZero(){
+        Point p1 = new Point(1, 100);
+        Point p2 = new Point(1, 100);
+        assertThat(p1).isEquivalentAccordingToCompareTo(p2);
+    }
+
+    @Test
+    public void compareTo_vertical_smallerYCoordinate_shouldBeLess(){
+        Point p1 = new Point(1, 10);
+        Point p2 = new Point(1, 100);
+        assertThat(p1).isLessThan(p2);
+    }
+
+    @Test
+    public void compareTo_vertical_largerYCoordinate_shouldBeGreater(){
+        Point p1 = new Point(1, 100);
+        Point p2 = new Point(1, 1);
+        assertThat(p1).isGreaterThan(p2);
+    }
+
+    @Test
+    public void compareTo_horizontal_smallerXCoordinate_shouldBeLess(){
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(20, 1);
+        assertThat(p1).isLessThan(p2);
+    }
+
+    @Test
+    public void compareTo_horizontal_largerXCoordinate_shouldBeGreater(){
+        Point p1 = new Point(30, 1);
+        Point p2 = new Point(29, 1);
+        assertThat(p1).isGreaterThan(p2);
+    }
+
+    @Test
+    public void compareTo_bothCoordinatesSmaller_shouldBeLess(){
+        Point p1 = new Point(20, 25);
+        Point p2 = new Point(120, 125);
+        assertThat(p1).isLessThan(p2);
+    }
+
+    @Test
+    public void compareTo_bothCoordinatesLarger_shouldBeLarger(){
+        Point p1 = new Point(120, 125);
+        Point p2 = new Point(20, 25);
+        assertThat(p1).isGreaterThan(p2);
+    }
+
+    @Test
+    public void compareTo_shouldBeTransitive(){
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(10, 20);
+        Point p3 = new Point(100, 200);
+
+        assertThat(p1).isLessThan(p2);
+        assertThat(p2).isLessThan(p3);
+        assertThat(p1).isLessThan(p3);
+    }
+
+    @Test
+    public void compareTo_shouldBeAntiSymmetric(){
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(10, 20);
+        assertThat(p1).isLessThan(p2);
+        assertThat(p2).isGreaterThan(p1);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Unit tests for slopeOrder()
+    ////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////
+    // Pairwise comparison
+    ///////////////////////////////////
+    @Test
+    public void slopeOrder_comparisonPointsAreSameAsSourcePoints_shouldBeEquivalent(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 1);
+        Comparator<Point> comparator = source.slopeOrder();
+        assertThat(comparator.compare(p1, p2)).isEqualTo(0);
+    }
+
+    @Test
+    public void slopeOrder_comparisonPointsAreEqualToEachOtherButDiffersFromSource_shouldBeEquivalent(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(2, 2);
+        Comparator<Point> comparator = source.slopeOrder();
+        assertThat(comparator.compare(p1, p2)).isEqualTo(0);
+    }
+
+    @Test
+    public void slopeOrder_point1IsVerticalToSource_pointTwoIsHorizontalToSource_pointWithHorizontalSlopeIsCloser(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(1, 20);
+        Point p2 = new Point(20, 1);
+        assertThat(source.slopeOrder().compare(p1, p2)).isEqualTo(1);
+    }
+
+    @Test
+    public void slopeOrder_slopeToOnePointSmallerThanSlopeToAnother_pointWithSmallerSlopeIsCloser(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(2, 20);
+        Point p2 = new Point(3, 3000);
+        assertThat(source.slopeOrder().compare(p1, p2)).isEqualTo(-1);
+    }
+
+    @Test
+    public void slopeOrder_slopeToOnePointLargerThanSlopeToAnother_pointWithLargerSlopeIsFarther(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(2, 200);
+        Point p2 = new Point(3, 4);
+        assertThat(source.slopeOrder().compare(p1, p2)).isEqualTo(1);
+    }
+
+    @Test
+    public void slopeOrder_allPointsOnHorizontalLine_pointWithSmallerXCoordinateIsCloser(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(2, 1);
+        Point p2 = new Point(3, 1);
+        assertThat(source.slopeOrder().compare(p1, p2)).isEqualTo(-1);
+    }
+
+    @Test
+    public void slopeOrder_allPointsOnVerticalLine_pointWithSmallerYCoordinateIsCloser(){
+        Point source = new Point(1, 1);
+        Point p1 = new Point(1, 500);
+        Point p2 = new Point(1, 700);
+        assertThat(source.slopeOrder().compare(p1, p2)).isEqualTo(-1);
+    }
+
+    ///////////////////////////////////
+    // Array-level comparison
+    ///////////////////////////////////
+    @Test
+    public void sortArrayBySlopeOrder_allPointsInArrayAreCollinear_sortByYThenByX(){
+        Point source = new Point(0, 0);
+        Point p1 = new Point(10, 10);
+        Point p2 = new Point(9, 9);
+        Point p3 = new Point(8, 8);
+        Point p4 = new Point(7, 7);
+        Point[] points = {p1, p2, p3, p4};
+        Arrays.sort(points, source.slopeOrder());
+
+        assertThat(points).asList().containsExactly(p4, p3, p2, p1).inOrder();
+    }
+
+    @Test
+    public void sortArrayBySlopeOrder_noPointsAreCollinear_sortBySlope(){
+        Point source = new Point(0, 0);
+        Point p1 = new Point(2, 5);
+        Point p2 = new Point(3, 100);
+        Point p3 = new Point(4, 3);
+        Point p4 = new Point(3, 9);
+        Point[] points = {p1, p2, p3, p4};
+        Arrays.sort(points, source.slopeOrder());
+
+        assertThat(points).asList().containsExactly(p3, p1, p4, p2).inOrder();
+    }
+
+    @Test
+    public void sortArrayBySlopeOrder_sortByDifferentSources_createsDifferentSorting(){
+        Point source1 = new Point(0, 0);
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 1);
+        Point p3 = new Point(3, 1);
+        Point p4 = new Point(4, 1);
+        Point[] points = {p1, p2, p3, p4};
+        Arrays.sort(points, source1.slopeOrder());
+        assertThat(points).asList().containsExactly(p4, p3, p2, p1).inOrder();
+
+        Point source2 = new Point(1, 1);
+        Arrays.sort(points, source2.slopeOrder());
+        assertThat(points).asList().containsExactly(p1, p2, p3, p4).inOrder();
+    }
+}
